f066fb87d6c885a3068c1a7139463c5970409fdc
tyautyau56 <i19yanagi@gmail.com>
2020-10-31 14:20:33 +0900

    add css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-//  productionが本番、developmentが開発用
-const MODE = "development"
+//  productionが本番、developmentが開発用(productionの時は利用しない)
+const enabledSourceMap = MODE === "development"
 
 module.exports = {
 
@@ -17,7 +17,21 @@ module.exports = {
             //  typescriptをts-loaderで変換
             {test: /\.tsx?$/, use: 'ts-loader'},
             //  css関係
-            {test: /\.css$/, use: ''},
+            {test: /\.css$/,
+                use: [
+                    //  linkタグに出力する機能
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            //  CSS内のurl()メソッドの取り込みを禁止
+                            url: false,
+                            //  ソースマップを有効にする
+                            sourceMap: enabledSourceMap
+                        }
+                    }
+                ]
+            },
         ],
     },
     resolve: {
