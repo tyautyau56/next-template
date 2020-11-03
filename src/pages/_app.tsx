bfcae9ff2f1569a5e73e13d8c1cdeac8b3235cc2
tyautyau56 <i19yanagi@gmail.com>
2020-11-03 20:37:41 +0900

    change src/page/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/main.scss'
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />
